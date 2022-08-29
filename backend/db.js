e5092a34c67db3d2b0227c778691e9363dc031c2
Vanashreeshankar <57714270+Vanashreeshankar@users.noreply.github.com>
2022-08-30 00:02:11 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Vanashree:Vana~22Shankar@cluster0.c9ygt.mongodb.net/blog_post?retryWrites=true&w=majority',(error) => {
+mongoose.connect('mongodb+srv://User_Name:Password@cluster0.c9ygt.mongodb.net/Collection?retryWrites=true&w=majority',(error) => {
     if(!error){
         console.log('Connection Successful')
     }else{
